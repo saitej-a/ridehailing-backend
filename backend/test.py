69e491cf0eeb2d3c66850415a426a39506e5c3fa
saitej-a <ankamsaiteja27@gmail.com>
2025-08-29 15:42:33 +0530

    working fine

--- backend/test.py
+++
@@ -1,5 +0,0 @@
-import requests as re
-import json
-
-req=re.post('http://localhost:8000/api/register/', data={'email': 'ankamsaiteja27+1@gmail.com','otp':'289658','password':'your_password','full_name':'Your Name','phone_number':'1234567890'})
-print(req.json())
